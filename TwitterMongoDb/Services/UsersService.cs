d3890d49902dd333594eaf0f16cc6db0b9b35003
onurd <106928484+onurdzd@users.noreply.github.com>
2023-08-30 12:44:37 +0300

    updates

--- TwitterMongoDb/Services/UsersService.cs
+++ TwitterMongoDb/Services/UsersService.cs
@@ -8,7 +8,7 @@ namespace TwitterMongoDb.Services
 {
     public class UsersService
     {
-        private readonly IMongoCollection<User> _booksCollection;
+        private readonly IMongoCollection<User> _usersCollection;
 
         public UsersService(
             IOptions<UsersStoreDatabaseSettings> userStoreDatabaseSettings)
@@ -23,23 +23,23 @@ namespace TwitterMongoDb.Services
             var mongoDatabase = mongoClient.GetDatabase(
                 config["DatabaseName"]);
 
-            _booksCollection = mongoDatabase.GetCollection<User>(
+            _usersCollection = mongoDatabase.GetCollection<User>(
                 config["UsersCollectionName"]);
         }
 
         public async Task<List<User>> GetAsync() =>
-            await _booksCollection.Find(_ => true).ToListAsync();
+            await _usersCollection.Find(_ => true).ToListAsync();
 
         public async Task<User?> GetAsync(string id) =>
-            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+            await _usersCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
         public async Task CreateAsync(User newUser) =>
-            await _booksCollection.InsertOneAsync(newUser);
+            await _usersCollection.InsertOneAsync(newUser);
 
         public async Task UpdateAsync(string id, User updatedUser) =>
-            await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+            await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
 
         public async Task RemoveAsync(string id) =>
-            await _booksCollection.DeleteOneAsync(x => x.Id == id);
+            await _usersCollection.DeleteOneAsync(x => x.Id == id);
     }
 }
